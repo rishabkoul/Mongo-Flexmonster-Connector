181a618a49e3aea7d5d67489c09ece7872ab15b1
Rishab Koul <rishabkoul2001@gmail.com>
2021-05-11 13:07:09 +0530

    deploy

---
+++ monog.js
@@ -0,0 +1,57 @@
+const mongo = require('express').Router();
+const mongodb = require("mongodb");
+const fm_mongo_connector = require("flexmonster-mongo-connector");
+
+let dbo = null;
+let _apiReference = null; // it’ll be the Connector instance
+
+mongodb.MongoClient.connect("mongodb+srv://admin:5eP6jc3GOC15BFnd@cluster0.eyj0g.mongodb.net/ecommerse?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+    }, (err, db) => {
+        if (err)
+            throw err;
+        dbo = db.db("ecommerse");
+        _apiReference = new fm_mongo_connector.MongoDataAPI();
+}); 
+
+// requests handling functions will appear here
+module.exports = mongo;
+
+mongo.post("/handshake", async (req, res) => {
+    try {
+        res.json({ version: _apiReference.API_VERSION });
+    } catch (err) {
+         handleError(err, res);
+    }
+});
+
+mongo.post("/fields", async (req, res) => {
+    try {
+        const result = await _apiReference.getSchema(dbo, req.body.index);
+        res.json(result);
+    } catch (err) {
+        //your error handler
+    }
+});
+
+mongo.post("/members", async (req, res) => {
+    try {
+        const result = await _apiReference.getMembers(
+            dbo, req.body.index, req.body.field, 
+            { page: req.body.page, pageToken: req.body.pageToken });
+        res.json(result);
+    } catch (err) {
+      //your error handler
+    }
+});
+
+mongo.post("/select", async (req, res) => {
+    try {
+        const result = await _apiReference.getSelectResult(
+            dbo, req.body.index, req.body.query,
+            { page: req.body.page, pageToken: req.body.pageToken });
+        res.json(result);
+    } catch (err) {
+      //your error handler
+    }
+});
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/mongo', require('./monog.js'));
+
+var port = 9204;
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}')
+});
